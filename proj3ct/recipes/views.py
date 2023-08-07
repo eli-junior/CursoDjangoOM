a3cf88d7230cd0ec1776dc10bf6e9274ae097625
Eli Júnior <elijunior.py@gmail.com>
2023-08-07 15:55:50 -0300

    aula 43

--- proj3ct/recipes/views.py
+++ proj3ct/recipes/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "recipes/pages/home.html")
+
+
+def recipe(request, _id):
+    print(_id)
+    return render(request, "recipes/pages/home.html")
