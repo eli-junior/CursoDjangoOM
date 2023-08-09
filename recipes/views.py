5fe7f7a8cb988d841c67e51fcdba240ffc33339b
Eli Júnior <elijunior.py@gmail.com>
2023-08-09 19:07:35 -0300

    aula 61

--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render
 
-from recipes.utils.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
     return render(
         request,
         "recipes/pages/home.html",
         context={
-            "recipes": make_recipe(10),
+            "recipes": recipes,
         },
     )
 
 
-def recipe(request, id):
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    return render(
+        request,
+        "recipes/pages/category.html",
+        context={
+            "recipes": recipes,
+            "category_name": recipes.first().category.name,
+        },
+    )
+
+
+def recipe(request, recipe_id):
+    recipe = Recipe.objects.filter(id=recipe_id, is_published=True).first()
     return render(
         request,
         "recipes/pages/recipe-view.html",
         context={
-            "recipe": make_recipe(),
+            "recipe": recipe,
             "is_detail_page": True,
         },
     )
