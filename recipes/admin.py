853e783f883c9fa44dd547f8992eff513438f238
F2804179 Eli Junior <eli.junior@bb.com.br>
2023-08-09 12:38:01 -0300

    Aula 55

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Category, Profile, Recipe
 
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    ...
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Profile, ProfileAdmin)
